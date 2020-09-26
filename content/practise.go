f59278bf623111be8636923bb6eed7c288b0d3c3
gaolinxiong <1906972240@qq.com>
2020-09-26 20:55:07 +0800

    feat: update

--- content/practise.go
+++ content/practise.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	//变量的两种命名方式
@@ -42,5 +45,109 @@ func main() {
 			break
 		}
 	}
+
+	b := []int{100, 101, 102}
+
+	for i, n :=range b {
+		println(i, "i:", n)
+	}
 	fmt.Println(a+x)
+	c, d := "10", 0
+	e, f := div(c, d)
+
+	fmt.Print(e, f)
+	test(1, 8)
+	sliceContent()
+	structure()
 }
+
+func div(a string, b int) (string, error) {
+	if b == 0 {
+		return a, errors.New("division by zero")
+	}
+
+	return a, nil
+}
+
+func test(a, b int) {
+	defer print("高林雄")
+
+	defer print("喜欢")
+
+	defer print("谁")
+
+	println(a / b, "gaolinxiong")
+}
+
+//切片及map
+func sliceContent() {
+	x := make([]int, 0, 5)
+
+	for i := 0; i < 11; i++ {
+		x = append(x, i)
+	}
+	println(x)
+
+	m := make(map[string]int)
+
+	m["a"] = 1
+
+	c, ok := m["b"]
+
+	fmt.Println(c, ok)
+
+	fmt.Println(m["a"])
+
+	delete(m, "a")
+}
+
+type user struct {
+	name string
+	age byte
+}
+
+type manager struct {
+	user
+	title string
+}
+
+func structure()  {
+	var m manager
+	m.name = "Tom"
+	m.age = 29
+	m.title = "CTO"
+
+	fmt.Println(m.user)
+
+	var x X
+	x.inc()
+	println("func", x)
+
+	var mo managerOne
+	mo.name = "gaolinxiong"
+	mo.age = 99
+	mo.title = "很帅"
+
+	println(mo.ToString())
+}
+
+type X int
+
+func (x *X) inc()  {
+	*x++
+}
+
+type userOne struct {
+	name string
+	age byte
+}
+
+func (u userOne) ToString() string {
+	return fmt.Sprint("%+v", u)
+}
+
+type managerOne struct {
+	userOne
+	title string
+}
+
