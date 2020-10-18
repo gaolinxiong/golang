6c084b30cc1643bc232293da702093d4e953cfc8
gaolinxiong <1906972240@qq.com>
2020-10-18 13:04:33 +0800

    feat: update

--- content/practise.go
+++ content/practise.go
@@ -6,91 +6,164 @@ import (
 	"time"
 )
 
+//var x = 100
 func main() {
+	
+	//x,_ := strconv.Atoi("123")
+	//println(_)
 	//变量的两种命名方式
-	var a int32 = 123
-	x := int32(321)
+	//var a int32 = 123
+	//x := int32(321)
+	//
+	////if else
+	//if x > 123 {
+	//	fmt.Println(a)
+	//} else if x == 123 {
+	//	print(1)
+	//} else {
+	//	print(0)
+	//}
+	//
+	//switch false {
+	//	case x < a:
+	//		println("错的")
+	//	case x > a:
+	//		println("对的")
+	//	default:
+	//		println("我错了")
+	//
+	//}
+	//
+	//for i := 0; i < 5; i++ {
+	//	println("i", i)
+	//}
+	//
+	//for a > 100 {
+	//	println('c', a)
+	//	a--
+	//}
+	//
+	//for {
+	//	println("while", a)
+	//	a++
+	//	if a == 123 {
+	//		break
+	//	}
+	//}
+	//
+	//b := []int{100, 101, 102}
+	//
+	//for i, n :=range b {
+	//	println(i, "i:", n)
+	//}
+	//fmt.Println(a+x)
+	//c, d := "10", 0
+	//e, f := div(c, d)
+	//
+	//fmt.Print(e, f)
+	//test(1, 8)
+	//sliceContent()
+	//structure()
+	//
+	//var u userTwo
+	//u.name = "Tom"
+	//u.age = 20
+	//
+	//var p Printer = u
+	//p.PrintUser()
+	//
+	//fmt.Println("-------------------")
+	//go task(1)
+	//
+	//go task(2)
+	//
+	//time.Sleep(time.Second * 5)
+	//
+	//ch2 := make(chan string, 1)
+	//// 下面就是传说中的通过启用一个Goroutine来并发的执行代码块的方法。
+	//// 关键字 go 后跟的就是需要被并发执行的代码块，它由一个匿名函数代表。
+	//// 对于 go 关键字以及函数编写方法，我们后面再做专门介绍。
+	//// 在这里，我们只要知道在花括号中的就是将要被并发执行的代码就可以了。
+	//go func() {
+	//	ch2 <- ("已到达")
+	//}()
+	//var value string = "数据"
+	//value = value + (<-ch2)
+	//fmt.Println(value)
 
-	//if else
-	if x > 123 {
-		fmt.Println(a)
-	} else if x == 123 {
-		print(1)
-	} else {
-		print(0)
+	//fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>")
+	//testFun()
+
+	//x := 345
+	//fmt.Println(&x)
+	//const x = 123
+	//println(&x)
+	//
+	//const y = 1.23
+	//
+	//{
+	//	x = 99
+	//	println(x)
+	//}
+	//const (
+	//	x, y int = 99, 1
+	//	b byte = byte(x)
+	//	n = uint8(y)
+	//)
+	//
+	//fmt.Println(b, y)
+	//
+	//s1 := "hello, world"
+	//s2 := `hello, world.
+	//send using Golang`
+	//fmt.Println(s1, s2)
+
+	//const (
+	//	f = 1 << iota
+	//	g
+	//	h
+	//)
+	//fmt.Println(f, g, h)
+
+	//start := time.Now()
+	//ch := make(chan int, 20)
+	//go testChan(ch)
+	//for i := range ch {
+	//	fmt.Println("接收到的数据:", i)
+	//}
+	//end := time.Now()
+	//consume := end.Sub(start).Seconds()
+	//fmt.Println("程序执行耗时(s)：", consume)
+
+	//pi := 3.14159
+	//// 按数值本身的格式输出
+	//variant := fmt.Sprintf("%v %v %v", "月球基地", pi, true)
+	//
+	//fmt.Println(variant)
+	// 匿名结构体声明, 并赋予初值
+	//profile := &struct {
+	//	Name string
+	//	HP   int
+	//}{
+	//	Name: "rat",
+	//	HP:   150,
+	//}
+	//
+	//fmt.Printf("使用'%%+v' %+v\n", profile)
+	//fmt.Printf("使用'%%#v' %#v\n", profile)
+	//fmt.Printf("使用'%%T' %T\n", profile)
+}
+
+func testChan(ch chan<- int) {
+	for i := 0; i < 100; i++ {
+		chda <- i
 	}
-
-	switch false {
-		case x < a:
-			println("错的")
-		case x > a:
-			println("对的")
-		default:
-			println("我错了")
-
-	}
-
-	for i := 0; i < 5; i++ {
-		println("i", i)
-	}
-
-	for a > 100 {
-		println('c', a)
-		a--
-	}
-
-	for {
-		println("while", a)
-		a++
-		if a == 123 {
-			break
-		}
-	}
-
-	b := []int{100, 101, 102}
-
-	for i, n :=range b {
-		println(i, "i:", n)
-	}
-	fmt.Println(a+x)
-	c, d := "10", 0
-	e, f := div(c, d)
-
-	fmt.Print(e, f)
-	test(1, 8)
-	sliceContent()
-	structure()
-
-	var u userTwo
-	u.name = "Tom"
-	u.age = 20
-
-	var p Printer = u
-	p.PrintUser()
-
-	fmt.Println("-------------------")
-	go task(1)
-
-	go task(2)
-
-	time.Sleep(time.Second * 5)
-
-	ch2 := make(chan string, 1)
-	// 下面就是传说中的通过启用一个Goroutine来并发的执行代码块的方法。
-	// 关键字 go 后跟的就是需要被并发执行的代码块，它由一个匿名函数代表。
-	// 对于 go 关键字以及函数编写方法，我们后面再做专门介绍。
-	// 在这里，我们只要知道在花括号中的就是将要被并发执行的代码就可以了。
-	go func() {
-		ch2 <- ("已到达")
-	}()
-	var value string = "数据"
-	value = value + (<-ch2)
-	fmt.Println(value)
-
-
+	close(ch)
 }
 
 func consumer(data chan int, done chan bool)  {
+	println("data", len(data))
+	println("done", done)
 	for x := range data {
 		println("recv", x)
 	}
@@ -99,8 +172,25 @@ func consumer(data chan int, done chan bool)  {
 }
 
 func producer(data chan int)  {
-	
+	for i:= 0; i < 4; i++ {
+		data <- i
+		time.Sleep(time.Second * 3)
+	}
+
+	close(data)
 }
+
+func testFun()  {
+	done := make(chan bool)
+	data := make(chan int)
+
+	go consumer(data, done)
+
+	go producer(data)
+
+	<-done
+}
+
 
 func task(id int) {
 	fmt.Println("测试", id)
@@ -212,4 +302,5 @@ func (u userTwo) PrintUser()  {
 type Printer interface {
 	PrintUser()
 }
+
 
